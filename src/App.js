21f9bc6ab890636893137994a4fb3ca22c3a516c
Luciano Bohrer <bohrerluciano@gmail.com>
2020-07-13 21:58:55 +0100

    Listing and deleting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepos(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    .then(() => {
+      const index = repos.findIndex( repo => repo.id === id);
+      if (index >= 0) {
+        const updateRepos = repos.filter(project => project.id !== id);
+        setRepos([...updateRepos]);
+      }
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repos.map(repo => (
+          <li>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
